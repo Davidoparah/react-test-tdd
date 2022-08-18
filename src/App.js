c0d77f5d077608e7d68b7f7f00574fc8416c876c
David Oparah <davidoparah@gmail.com>
2022-08-18 14:24:03 +0100

    made several changes to fet first test running

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from './components/button/button';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/button/button";
 
 function App() {
   return (
